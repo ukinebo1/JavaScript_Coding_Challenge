aebdd216ef9502424368984051494a9def1d744d
Ukinebo1 <perfectprecious2022@gmail.com>
2024-06-26 03:42:29 +0100

    Completed the level 3 exercise of day 4

--- Day4/Exercise3.js
+++ Day4/Exercise3.js
@@ -17,3 +17,50 @@
 //   ```
 
 // 1. Write a program which tells the number of days in a month, now consider leap year.
+
+let user = prompt("Enter a month")
+let month =user.toLowerCase()
+
+switch (month) {
+    case "january":
+        alert(`${month} has 31 days.`)
+        break;
+        case "february":
+        alert(`${month} has 28 days.`)
+        break;
+        case "march":
+        alert(`${month} has 31 days.`)
+        break;
+        case "april":
+        alert(`${month} has 30 days.`)
+        break;
+        case "may":
+        alert(`${month} has 31 days.`)
+        break;
+        case "june":
+        alert(`${month} has 30 days.`)
+        break;
+        case "july":
+        alert(`${month} has 31 days.`)
+        break;
+        case "august":
+        alert(`${month} has 31 days.`)
+        // return (`${month} has 31 days.`)
+        break;
+        case "september":
+        alert(`${month} has 30 days.`)
+        break;
+        case "october":
+        alert(`${month} has 31 days.`)
+        break;
+        case "november":
+        alert(`${month} has 31 days.`)
+        break;
+        case "december":
+        alert(`${month} has 31 days.`)
+        break;
+
+    default:
+        alert(`Invalid month`)
+        break;
+}
